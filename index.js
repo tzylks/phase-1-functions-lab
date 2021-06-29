e0d7a71910003317981ba03560f0fb6d6b95dfe3
TrevorZylks <trevorzylks@gmail.com>
2021-06-28 17:09:44 -0700

    commit tree 2dd696d27935ed487c9261f55dfff86906db3d13

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+const address = 42
+const feet = 264
+function distanceFromHqInBlocks(destination){
+  if (destination >= 42) {
+      return destination - address;
+}
+  else if (destination <= 42) {
+      return address - destination;
+    }
+ }
+
+ function distanceFromHqInFeet (someValue) {
+    return distanceFromHqInBlocks(someValue) * feet;
+  } 
+
+  function distanceTravelledInFeet(start, end) {
+      let blocksTrav = Math.abs(start - end);
+      return blocksTrav * feet;
+  }
+
+  function calculatesFarePrice(start, end) {
+    const distTravel = distanceTravelledInFeet(start, end);
+    if (distTravel > 400 && distTravel < 2000){
+            return (distTravel - 400) * 2 / 100;
+    } else if (distTravel > 2000 && distTravel< 2500) {
+        return 25;
+    } else if (distTravel < 400) {
+        return 0;
+    } else if (distTravel > 2500) {
+        return "cannot travel that far";
+    }
+  }
